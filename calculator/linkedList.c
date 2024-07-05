6b2caec2b8e45d54dcb099afb1c82a57f11a46a5
wndudrla1011 <wndudrla1011@naver.com>
2024-07-05 10:49:57 +0900

    passed test round calc double

--- calculator/linkedList.c
+++ calculator/linkedList.c
@@ -117,14 +117,6 @@ char *cstrtok(char *postfix, char *delim)
     return start;
 }
 
-double cround(double num)
-{
-    int n = 0;
-    if (num >= 0)
-    {
-    }
-}
-
 void toPostfix(char exp[], char postfix[])
 {
     int p = 0; // p : postfix 배열 인덱스
@@ -242,7 +234,6 @@ double calculate(char postfix[])
 
 int main(void)
 {
-    int n = 0;           // 반올림 자리
     double result = 0.0; // 계산 결과
     char *exp;           // 입력
     char postfix[MAX_SIZE] = {'\0'};
@@ -254,11 +245,7 @@ int main(void)
     toPostfix(exp, postfix);
     printf("\nPostfix: %s\n", postfix);
 
-    // printf("몇 번째 자리에서 반올림하시겠습니까 >>> ");
-    // scanf("%d", &n);
-
     result = calculate(postfix);
-    // result = result + pow(10, -n) * 5; // 소수점 n자리 반올림
 
     printf("계산한 결과: \t%.3f\n", result);
 
