cdaa1012a2d24c6acce8c3103c7a4697ef730d3f
wndudrla1011 <wndudrla1011@naver.com>
2024-07-09 15:51:23 +0900

    passed test chatting 1:1

--- chat/client.c
+++ chat/client.c
@@ -1,108 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <winsock2.h>
 #include <unistd.h>
-/* Linux
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <sys/un.h>
-*/
+// #include <arpa/inet.h>
+// #include <sys/socket.h>
 #include <pthread.h>
-#define BUF_SIZE 100
-#define NAME_SIZE 20
 
-void error_handling(char *message);
-void *send_msg(void *arg);
-void *recv_msg(void *arg);
-
-char name[NAME_SIZE] = "[DEFAULT]";
-char msg[BUF_SIZE];
-
-int main(int argc, char **argv)
+typedef struct recieve_data
 {
-    int sock = 0;                   // socket descriptor
-    struct sockaddr_in server_addr; // 서버 소켓 정보
-    pthread_t send_thread, recv_thread;
+    char *message;
+    int *serv_sock;
+} recieve_data;
 
-    if (argc != 4)
+void *send_function(void *serv_sock)
+{
+    int *cs = (int *)serv_sock;
+    while (1)
     {
-        printf("Usage : %s <IP> <PORT> <NAME>\n", argv[0]);
+        char message[30];
+        printf("\nclient -> : ");
+        scanf("%s", message);
+        write(*cs, message, sizeof(message));
+    }
+}
+
+void *recieve_function(void *rcvDt)
+{
+
+    recieve_data *data = (recieve_data *)rcvDt;
+    char *msg = data->message;
+    while (1)
+    {
+        int str_len = read(*(data->serv_sock), msg, sizeof(char) * 30);
+        if (str_len != -1)
+        {
+            printf("\n <- server : %s\n", msg);
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+
+    int serv_sock;
+    struct sockaddr_in serv_addr;
+    char message[30];
+    int str_len;
+
+    if (argc != 3)
+    {
+        printf("Usage : %s <IP> <port>\n", argv[0]);
         exit(1);
     }
 
-    sprintf(name, "[%s]", argv[3]);
-    sock = socket(AF_INET, SOCK_STREAM, 0); // 소켓 생성
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(atoi(argv[2]));
 
-    if (sock == -1)
+    connect(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+    pthread_t p_thread[2];
+    int t;
+    int status;
+    recieve_data rcvDt;
+    rcvDt.message = message;
+    rcvDt.serv_sock = &serv_sock;
+    for (t = 0; t < 2; t++)
     {
-        error_handling("socket() error");
+        if (t == 0)
+            pthread_create(&p_thread[t], NULL, send_function, (void *)&serv_sock);
+        else if (t == 1)
+            pthread_create(&p_thread[t], NULL, recieve_function, (void *)&rcvDt);
     }
+    pthread_join(p_thread[0], (void **)&status);
+    pthread_join(p_thread[1], (void **)&status);
 
-    // 소켓 값 생성
-    memset(&server_addr, 0, sizeof(server_addr)); // 서버 소켓 메모리 할당
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(argv[1]); // dotted decimal -> unsigned long
-    server_addr.sin_port = htons(atoi(argv[2]));      // atoi (char -> integer)
-
-    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) // 서버에 연결 요청
-    {
-        error_handling("connect() error");
-    }
-
-    pthread_create(&send_thread, NULL, send_msg, (void *)&sock);
-    pthread_create(&recv_thread, NULL, recv_msg, (void *)&sock);
-    pthread_join(send_thread, NULL);
-    pthread_join(recv_thread, NULL);
-
-    close(sock); // 클라이언트 소켓 Close
-
+    close(serv_sock);
     return 0;
-}
-
-void error_handling(char *message)
-{
-    fputs(message, stderr);
-    fputc('\n', stderr);
-    exit(1);
-}
-
-void *send_msg(void *arg)
-{
-    int sock = *((int *)arg);
-    char name_msg[NAME_SIZE + BUF_SIZE];
-    while (1)
-    {
-        fgets(msg, BUF_SIZE, stdin);
-        if (!strcmp(msg, "q\n") || !strcmp(msg, "Q\n"))
-        {
-            close(sock);
-            // exit(0)가 아닌 return NULL로 하면 아직 recv_msg 스레드가 살아있어서 바로 종료되지 않는다
-            exit(0);
-        }
-        sprintf(name_msg, "%s %s", name, msg);
-        write(sock, name_msg, strlen(name_msg));
-    }
-
-    return NULL;
-}
-
-void *recv_msg(void *arg)
-{
-    int sock = *((int *)arg);
-    char name_msg[NAME_SIZE + BUF_SIZE];
-    int str_len;
-    while (1)
-    {
-        str_len = read(sock, name_msg, sizeof(name_msg) - 1);
-        if (str_len < 0)
-        {
-            return NULL;
-        }
-        name_msg[str_len] = 0;
-        fputs(name_msg, stdout);
-    }
-
-    return NULL;
 }
--- chat/server.c
+++ chat/server.c
@@ -1,86 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <winsock2.h>
 #include <unistd.h>
-/* Linux
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <sys/un.h>
-*/
+// #include <arpa/inet.h>
+// #include <sys/socket.h>
 #include <pthread.h>
-#define BUF_SIZE 100
-#define MAX_CLNT 256
-
 void error_handling(char *message);
-void *handle_client(void *arg);
-void send_msg(char *msg, int len);
 
-int client_cnt = 0;
-int client_socks[MAX_CLNT];
-pthread_mutex_t mtx;
-
-int main(int argc, char **argv)
+typedef struct
 {
-    int server_sock;                // server socket descriptor
-    int client_sock;                // client socket descriptor
-    struct sockaddr_in server_addr; // 서버 소켓 정보
-    struct sockaddr_in client_addr; // 클라이언트 소켓 정보
-    int client_addr_size;
-    // socklen_t client_addr_size; -> Linux
-    pthread_t tid;
+    int *clnt_sock;
+    char *message;
+} recieve_Data;
 
+void *send_function(void *clnt_sock)
+{
+    int *cs = (int *)clnt_sock;
+    while (1)
+    {
+        char message[30];
+        printf("\nserver -> : ");
+        scanf("%s", message);
+        write(*cs, message, sizeof(message));
+    }
+}
+
+void *recieve_function(void *rcvdt)
+{
+    recieve_Data *data = (recieve_Data *)rcvdt;
+    while (1)
+    {
+        int str_len = read(*(data->clnt_sock), data->message, sizeof(char) * 30);
+        if (str_len != -1)
+        {
+            printf("\n <- client : %s\n", data->message);
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int serv_sock;
+    int clnt_sock;
+    char message[30];
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_size;
+
+    printf("read port....\n");
     if (argc != 2)
     {
         printf("Usage : %s <port>\n", argv[0]);
         exit(1);
     }
 
-    pthread_mutex_init(&mtx, NULL);
-    server_sock = socket(AF_INET, SOCK_STREAM, 0); // 소켓 생성
-
-    if (server_sock == -1)
+    printf("set server socket\n");
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (serv_sock == -1)
     {
         error_handling("socket() error");
     }
 
-    // 소켓 값 바인딩
-    memset(&server_addr, 0, sizeof(server_addr)); // 서버 소켓 메모리 할당
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons(atoi(argv[1])); // atoi (char -> integer)
+    printf("set server addr...\n");
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
 
-    if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) // 서버 소켓 바인딩
+    printf("binding...\n");
+    if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
     {
         error_handling("bind() error");
     }
 
-    if (listen(server_sock, 5) == -1) // 클라이언트 요청 대기
+    if (listen(serv_sock, 5) == -1)
     {
         error_handling("listen() error");
     }
+    printf("waiting...\n");
 
-    while (1)
+    clnt_addr_size = sizeof(clnt_addr);
+    clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
+    if (clnt_sock == -1)
+        error_handling("accept() error");
+    printf("accept!\n");
+
+    recieve_Data rcvdt;
+    rcvdt.clnt_sock = &clnt_sock;
+    rcvdt.message = message;
+
+    pthread_t p_thread[2];
+    int t;
+    int status;
+    for (t = 0; t < 2; t++)
     {
-        client_addr_size = sizeof(client_addr);
-        client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_addr_size); // 클라이언트 요청 수락
-        if (client_sock == -1)
-        {
-            error_handling("accept() error");
-        }
-
-        pthread_mutex_lock(&mtx);
-        client_socks[client_cnt++] = client_sock;
-        pthread_mutex_unlock(&mtx);
-
-        pthread_create(&tid, NULL, handle_client, (void *)&client_sock);
-        pthread_detach(tid);
-        printf("Connected client IP : %s\n", inet_ntoa(client_addr.sin_addr));
+        if (t == 0)
+            pthread_create(&p_thread[t], NULL, send_function, (void *)&clnt_sock);
+        else if (t == 1)
+            pthread_create(&p_thread[t], NULL, recieve_function, (void *)&rcvdt);
     }
 
-    close(server_sock); // 서버 소켓 Close
+    pthread_join(p_thread[0], (void **)&status);
+    pthread_join(p_thread[1], (void **)&status);
 
+    close(clnt_sock);
+    close(serv_sock);
     return 0;
 }
 
@@ -89,46 +113,4 @@ void error_handling(char *message)
     fputs(message, stderr);
     fputc('\n', stderr);
     exit(1);
-}
-
-void *handle_client(void *arg)
-{
-    int client_sock = *((int *)arg);
-    int str_len = 0;
-    char msg[BUF_SIZE];
-
-    // 클라이언트가 close를 날린다면 EOF가 도달해서 read 함수가 0을 반환한다
-    while (str_len = read(client_sock, msg, sizeof(msg)) != 0)
-    {
-        send_msg(msg, str_len);
-    }
-
-    // 클라이언트
-    pthread_mutex_lock(&mtx);
-    for (int i = 0; i < client_cnt; i++)
-    {
-        if (client_sock == client_socks[i])
-        {
-            while (++i < client_cnt)
-            {
-                client_socks[i - 1] = client_socks[i];
-            }
-            break;
-        }
-    }
-    client_cnt--;
-    pthread_mutex_unlock(&mtx);
-    close(client_sock);
-
-    return NULL;
-}
-
-void send_msg(char *msg, int len)
-{
-    pthread_mutex_lock(&mtx);
-    for (int i = 0; i < client_cnt; i++)
-    {
-        write(client_socks[i], msg, len);
-    }
-    pthread_mutex_unlock(&mtx);
 }
