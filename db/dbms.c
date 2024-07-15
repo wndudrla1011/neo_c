3fb75bef19efd142efb7a42c7cb622b072b24e72
wndudrla1011 <wndudrla1011@naver.com>
2024-07-15 14:56:59 +0900

    edited cstrtok -> strtok

--- db/dbms.c
+++ db/dbms.c
@@ -1,43 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
-int cstrlen(char *str)
-{
-    int idx = 0;
-    while (str[idx] != '\0')
-        ++idx;
-    return idx;
-}
-
-char *cstrtok(char *query, char *delim)
-{
-    char *start = 0; // 문자열 시작 위치
-    int i = 0;
-    static char *tmp; // 문자열 주소 저장
-
-    if (query != NULL) // 첫 토큰 받을 때
-        start = query;
-    else // 두 번째 이후 토큰 받을 때
-        start = tmp;
-
-    if (cstrlen(start) < 1) // 문자열 종료
-        return NULL;
-
-    for (i = 0; i < cstrlen(start); i++)
-    {
-        if (start[i] == (*delim) || start[i] == '\0')
-        {
-            start[i] = '\0';
-            break;
-        }
-    }
-
-    tmp = &start[i + 1]; // 다음 배열 시작 위치
-
-    return start;
-}
+#define QUERY_SIZE 100
 
 int main(void)
 {
-    printf("test");
+    char query[QUERY_SIZE]; // 입력 쿼리
+    char *delim = " ";      // delimiter
+    char *token;
+
+    fgets(query, QUERY_SIZE, stdin); // 쿼리 저장
+
+    token = strtok(query, delim);
+
+    while (token != NULL)
+    {
+        printf("token: %s\n", token);
+        token = strtok(NULL, delim);
+    }
+
     return 0;
 }
