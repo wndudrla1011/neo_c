bc14b1f5e9657443692c559bb8e6666a8ca9a4a4
wndudrla1011 <wndudrla1011@naver.com>
2024-07-17 10:28:28 +0900

    passed test "create table" and "show tables"

--- db/dbms.c
+++ db/dbms.c
@@ -18,8 +18,20 @@ typedef struct Table
 {
     char tname[MAX_INPUT];                  // 테이블명
     int cadinality;                         // 튜블 개수 == tuple 스택의 top
+    struct Table *next;                     // 다음 테이블 포인터
     char *tuple[MAX_CADINALITY][MAX_INPUT]; // 튜플 선언
 } Table;
+
+//>>>>>>>>>>>>>>>>>>>>>>>DB 관련 메서드>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+DB *init_db() // DB 목록의 head 생성
+{
+    DB *head;
+    head = (DB *)malloc(sizeof(DB));
+    head->next = NULL;
+
+    return (head);
+}
 
 int get_cnt_db(DB *db) // 생성된 DB 개수
 {
@@ -33,15 +45,6 @@ int get_cnt_db(DB *db) // 생성된 DB 개수
     }
 
     return cnt;
-}
-
-DB *init_db() // DB 목록의 head 생성
-{
-    DB *head;
-    head = (DB *)malloc(sizeof(DB));
-    head->next = NULL;
-
-    return (head);
 }
 
 DB *find_end_db(DB *db) // 가장 마지막 DB 찾기
@@ -60,11 +63,11 @@ DB *find_end_db(DB *db) // 가장 마지막 DB 찾기
 void add_db(DB *db, char *name) // 마지막 노드에 새 DB 추가
 {
     DB *end;
-    end = find_end_db(db); // Leaf Node
+    end = find_end_db(db); // Leaf DB
     DB *new_db;
     new_db = (DB *)malloc(sizeof(DB)); // 새 DB 생성
-    strcpy(new_db->dname, name);
-    end->next = new_db; // last db -> new db
+    strcpy(new_db->dname, name);       // DB명 입력
+    end->next = new_db;                // last db -> new db
     new_db->next = NULL;
 }
 
@@ -87,42 +90,88 @@ void print_all_db(DB *db) // 모든 DB 출력
 
             cur = cur->next;
         }
-        printf("======================================\n");
+        printf("======================================\n\n");
     }
 }
 
-char *getData(char *str, char *delim)
+//>>>>>>>>>>>>>>>>>>>>>>>Table 관련 메서드>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+Table *init_table(DB *db) // Table 목록의 head 생성
 {
-    char *data = NULL;
-    int i = 0;
-    static char *next;
+    Table *head;
+    head = (Table *)malloc(sizeof(Table));
+    head->next = NULL;
+    db->thead = head; // DB의 Table head 설정
 
-    if (str != NULL)
-        data = str;
-    else
-        data = next;
+    return (head);
+}
 
-    if (strlen(data) < 1)
-        return NULL;
+int get_cnt_table(Table *T) // 생성된 Table 개수
+{
+    int cnt = 0;
+    Table *cur = T;
 
-    for (i = 0; i < strlen(data); i++)
+    while (cur != NULL)
     {
-        if (data[i] == (*delim) || data[i] == '\0')
-        {
-            data[i] = '\0';
-            break;
-        }
+        cur = cur->next;
+        cnt++;
     }
 
-    next = &data[i + 1];
+    return cnt;
+}
 
-    return data;
+Table *find_end_table(Table *T) // DB의 Table 목록에서 가장 마지막 Table 찾기
+{
+    Table *cur;
+    cur = T;
+
+    while (cur->next != NULL)
+    {
+        cur = cur->next;
+    }
+
+    return (cur);
+}
+
+void add_table(DB *db, Table *table, char *name) // DB의 Table 목록 마지막 노드에 새 Table 추가
+{
+    Table *end;
+    end = find_end_table(table); // Leaf Table
+    Table *new_table;
+    new_table = (Table *)malloc(sizeof(Table)); // 새 Table 생성
+    strcpy(new_table->tname, name);             // 테이블명 입력
+    end->next = new_table;                      // last table -> new table
+    new_table->next = NULL;
+    db->tcnt++; // DB의 테이블 개수 증가
+}
+
+void print_all_table(DB *db) // 모든 Table 출력
+{
+    Table *table;
+    table = db->thead->next;
+
+    if (table != NULL)
+    {
+        printf("======================================");
+        while (1)
+        {
+            printf("\n%s", table->tname);
+            if (table->next == NULL)
+            {
+                printf("\n");
+                break;
+            }
+
+            table = table->next;
+        }
+        printf("======================================\n\n");
+    }
 }
 
 int main(void)
 {
     DB *db = NULL;
-    Table table;
+    Table *table = NULL;
     char input[MAX_INPUT]; // 입력 값
     char *command;         // 명령어
 
@@ -141,24 +190,53 @@ int main(void)
                 if (get_cnt_db(db) == 0) // 생성한 DB가 없을 때
                 {
                     printf("No database exist\n");
-                    exit(1);
+                    continue;
                 }
                 else
                 {
                     print_all_db(db); // 생성된 DB 출력
                 }
             }
+
+            else if (!strcmp(command, "tables") || !strcmp(command, "TABLES")) // Query > show table
+            {
+                if (get_cnt_table(table) == 0) // 생성한 Table이 없을 때
+                {
+                    printf("Entry empty\n");
+                    continue;
+                }
+                else
+                {
+                    print_all_table(db); // 생성된 Table 출력
+                }
+            }
         }
         else if (!strcmp(command, "create") || !strcmp(command, "CREATE"))
         {
             command = strtok(NULL, " ");
+
             if (!strcmp(command, "database") || !strcmp(command, "DATABASE")) // Query > create database
             {
-                if (get_cnt_db(db) == 0)
+                if (get_cnt_db(db) == 0) // 첫 DB 생성
                 {
                     db = init_db(); // DB 초기화
                 }
                 add_db(db, strtok(NULL, ";")); // 연결 리스트 -> New DB
+                printf("Query Success!\n");
+            }
+
+            else if (!strcmp(command, "table") || !strcmp(command, "TABLE")) // Query > create table
+            {
+                if (get_cnt_table(table) == 0) // 첫 Table 생성
+                {
+                    if (db == NULL)
+                    {
+                        printf("No database selected\n");
+                        continue;
+                    }
+                    table = init_table(db); // Table 초기화
+                }
+                add_table(db, table, strtok(NULL, ";")); // 연결 리스트 -> New Table
                 printf("Query Success!\n");
             }
         }
