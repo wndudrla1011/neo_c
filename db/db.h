0f77bb18ae91b05a6bb2ca455424e44dc0882e88
wndudrla1011 <wndudrla1011@naver.com>
2024-07-19 11:10:37 +0900

    separate functions related db and table as header file

---
+++ db/db.h
@@ -0,0 +1,132 @@
+#ifndef DB_H
+#define DB_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_INPUT 100 // 최대 입력 값 길이
+
+typedef struct DB
+{
+    char dname[MAX_INPUT]; // DB명
+    int tcnt;              // 테이블 개수
+    struct DB *next;       // 다음 DB 포인터
+    struct Table *thead;   // 테이블 목록 (Table 순차 검색)
+} DB;
+
+DB *init_db() // DB 목록의 head 생성
+{
+    DB *head;
+    head = (DB *)malloc(sizeof(DB));
+    head->next = NULL;
+
+    return (head);
+}
+
+int get_cnt_db(DB *h) // 생성된 DB 개수
+{
+    if (h == NULL) // DB를 생성하기 전 상황
+        return 0;
+
+    int cnt = -1;
+    DB *cur = h;
+
+    while (cur != NULL)
+    {
+        cur = cur->next;
+        cnt++;
+    }
+
+    return cnt;
+}
+
+DB *find_end_db(DB *db) // 가장 마지막 DB 찾기
+{
+    DB *cur;
+    cur = db;
+
+    while (cur->next != NULL)
+    {
+        cur = cur->next;
+    }
+
+    return (cur);
+}
+
+void add_db(DB *db, char *name) // 마지막 노드에 새 DB 추가
+{
+    DB *end;
+    end = find_end_db(db); // Leaf DB
+    DB *new_db;
+    new_db = (DB *)malloc(sizeof(DB)); // 새 DB 생성
+    strcpy(new_db->dname, name);       // DB명 입력
+    end->next = new_db;                // last db -> new db
+    new_db->next = NULL;
+}
+
+void print_all_db(DB *h) // 모든 DB 출력
+{
+    DB *cur;
+    cur = h->next;
+
+    if (cur != NULL)
+    {
+        printf("======================================");
+        while (1)
+        {
+            printf("\n%s", cur->dname);
+            if (cur->next == NULL)
+            {
+                printf("\n");
+                break;
+            }
+
+            cur = cur->next;
+        }
+        printf("======================================\n\n");
+    }
+}
+
+DB *read_db(DB *h, char *dname) // DB 이름으로 DB 찾기
+{
+    if (h == NULL) // DB를 생성하기 전 상황
+        return (h);
+
+    DB *cur;
+    cur = h;
+
+    while (strcmp(cur->dname, dname) && cur->next != NULL)
+    {
+        cur = cur->next;
+    }
+
+    if (cur == NULL) // Not found DB
+    {
+        printf("Unknown database '%s'\n", dname);
+        return (h);
+    }
+
+    return (cur);
+}
+
+void delete_db(DB *h, char *name) // DB 삭제
+{
+    DB *prev;
+    DB *cur; // 삭제할 DB
+    cur = h;
+
+    while (strcmp(cur->dname, name) && cur->next != NULL)
+    {
+        if (!strcmp(cur->next->dname, name))
+            prev = cur;
+        cur = cur->next;
+    }
+
+    prev->next = cur->next;
+    cur->next = NULL;
+
+    free(cur);
+}
+
+#endif
---
+++ db/table.h
@@ -0,0 +1,111 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_INPUT 100 // 최대 입력 값 길이
+
+typedef struct Table
+{
+    char tname[MAX_INPUT]; // 테이블명
+    int cadinality;        // 튜블 개수 == tuple 스택의 top
+    struct Table *next;    // 다음 테이블 포인터
+    char *Domain;          // 튜플 선언
+} Table;
+
+Table *init_table(DB *db) // Table 목록의 head 생성
+{
+    Table *head;
+    head = (Table *)malloc(sizeof(Table));
+    head->next = NULL;
+    db->thead = head; // DB의 Table head 설정
+
+    return (head);
+}
+
+Table *find_end_table(Table *T) // DB의 Table 목록에서 가장 마지막 Table 찾기
+{
+    Table *cur;
+    cur = T;
+
+    while (cur->next != NULL)
+    {
+        cur = cur->next;
+    }
+
+    return (cur);
+}
+
+void add_table(DB *db, Table *table, char *name) // DB의 Table 목록 마지막 노드에 새 Table 추가
+{
+    Table *end;
+    end = find_end_table(table); // Leaf Table
+    Table *new_table;
+    new_table = (Table *)malloc(sizeof(Table)); // 새 Table 생성
+    strcpy(new_table->tname, name);             // 테이블명 입력
+    end->next = new_table;                      // last table -> new table
+    new_table->next = NULL;
+    db->tcnt++; // DB의 테이블 개수 증가
+}
+
+void print_all_table(DB *db) // 모든 Table 출력
+{
+    Table *cur;
+    cur = db->thead->next;
+
+    if (cur != NULL)
+    {
+        printf("======================================");
+        while (1)
+        {
+            printf("\n%s", cur->tname);
+            if (cur->next == NULL)
+            {
+                printf("\n");
+                break;
+            }
+
+            cur = cur->next;
+        }
+        printf("======================================\n\n");
+    }
+}
+
+void delete_all_table(DB *db) // 모든 테이블 삭제
+{
+    Table *cur;
+    cur = db->thead;
+
+    Table *next;
+    while (cur != NULL)
+    {
+        next = cur->next;
+
+        free(cur);
+        cur = next;
+    }
+    free(db->thead);
+}
+
+void delete_table(DB *db, char *name) // 테이블 삭제
+{
+    Table *prev;
+    Table *cur; // 삭제할 테이블
+    cur = db->thead;
+
+    while (strcmp(cur->tname, name) && cur->next != NULL)
+    {
+        if (!strcmp(cur->next->tname, name))
+            prev = cur;
+        cur = cur->next;
+    }
+
+    prev->next = cur->next;
+    cur->next = NULL;
+
+    free(cur);
+    db->tcnt--; // DB의 Table 개수 감소
+}
+
+#endif
