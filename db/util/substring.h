d4611a29cad9aed18e3b7b5b211d1dc0d4d65e29
wndudrla1011 <wndudrla1011@naver.com>
2024-07-29 11:46:20 +0900

    handling parsing error in substring()

--- db/util/substring.h
+++ db/util/substring.h
@@ -4,10 +4,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *substring(int s, int e, char *str) //s: 시작 위치, e: 끝 위치, str: 작업할 문자열
+char *substring(int s, int e, char *str) // s: 시작 위치, e: 끝 위치, str: 작업할 문자열
 {
     char *unpack = (char *)malloc(sizeof(char) * (e - s + 2));
     strncpy(unpack, str + s, e - s + 1); // s 부터 (s ~ e) 길이만큼 저장
+    unpack[e] = '\0';
 
     return (unpack);
 }
