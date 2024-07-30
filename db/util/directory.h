93c4fb9de1ec77db78b18a7358ea21d9a0027c5e
wndudrla1011 <wndudrla1011@naver.com>
2024-07-30 19:58:27 +0900

    added createDirectory() and directoryExists() and renameDirectory()

--- db/util/directory.h
+++ db/util/directory.h
@@ -2,10 +2,13 @@
 #define DIRECTORY_H
 
 #include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
-int directoryExists(const char* dirName) {
+int directoryExists(const char* dirName) { //디렉토리 존재 여부 확인
     struct stat info;
     
     if (stat(dirName, &info) != 0) {
@@ -23,6 +26,18 @@ int createDirectory(const char* dirName) { // 디렉토리 생성
         return 0;
     } else {
         perror("디렉토리 생성 실패");
+        return -1;
+    }
+}
+
+int renameDirectory(const char *oldName, const char *newName) //디렉토리 이름 변경
+{
+    if (!rename(oldName, newName))
+    {
+        printf("디렉토리 이름 변경 성공: %s -> %s\n", oldName, newName);
+        return 0;
+    } else {
+        perror("디렉토리 이름 변경 실패");
         return -1;
     }
 }
